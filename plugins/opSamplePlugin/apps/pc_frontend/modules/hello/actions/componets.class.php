492fe9a74094effdecbcab603e1774ceb34eb7c2
tsakai <atm591.@gmail.com>
2010-09-22 13:26:36 +0900

    commit test1

---
+++ plugins/opSamplePlugin/apps/pc_frontend/modules/hello/actions/componets.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * ashiato components.
+ *
+ * @package    OpenPNE
+ * @subpackage ashiato
+ * @author     uechoco
+ */
+class ashiatoComponents extends sfComponents
+{
+ 
+  public function executeAshiatoHomeList()
+  {
+    $this->id = $this->getRequestParameter('id', $this->getUser()->getMemberId());
+    $this->pager = AshiatoPeer::getAshiatoMemberListPager($this->id, 1, 5);
+  }
+}
